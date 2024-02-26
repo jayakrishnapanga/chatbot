a1e0886bfd1f93c65ef92983e193cb3483975a4a
Panga Jayakrishna <pangajayakrishna@Pangas-MacBook-Air.local>
2024-02-27 00:43:09 +0530

    added backend endpoint

--- src/App.js
+++ src/App.js
@@ -268,7 +268,7 @@ function App() {
         addUserMessage(input);
       }
   
-      const response = await axios.post('https://chatbotbackend-drab.vercel.app/send-message', {
+      const response = await axios.post('https://chatbotbackend-delta.vercel.app/send-message', {
         recentMessage: chatMessages.length > 0 ? chatMessages[chatMessages.length - 1].message : null,
         currentMessage: input,
       });
